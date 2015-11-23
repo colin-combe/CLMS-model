8f8d54506a1892ff473bb0380677752cb7543c4e
Colin Combe <colin.combe@ed.ac.uk>
2015-11-23 14:46:07 +0000

    built

--- build/build.sh
+++ build/build.sh
@@ -4,18 +4,11 @@ java -jar compiler.jar --version
 #versioning system consists of editing numbers in file name in last line
 java -jar compiler.jar \
 --js=../src/controller/Init.js \
---js=../src/controller/MouseEvents.js \
---js=../src/controller/TouchEvents.js \
---js=../src/controller/Layout.js \
---js=../src/controller/Refresh.js \
---js=../src/controller/ToolTips.js \
 --js=../src/model/Match.js \
---js=../src/model/Link.js \
 --js=../src/model/Protein.js \
 --js=../src/model/Annotation.js \
 --js=../src/model/ProteinLink.js \
---js=../src/model/ResidueLink.js \
+--js=../src/model/CrossLink.js \
 --js=../src/controller/xiNET_Storage.js \
---js=../src/controller/Rotator.js \
 --js=../src/controller/ReadCSV.js \
---js_output_file=./crosslinkviewer.js;
+--js_output_file=./CLMS_model.js;
