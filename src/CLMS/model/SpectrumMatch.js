49e80a148079de7d8563cb15ca230f882bfc9714
Colin Combe <colin.combe@ed.ac.uk>
2019-02-06 15:02:07 +0000

    fix

--- src/CLMS/model/SpectrumMatch.js
+++ src/CLMS/model/SpectrumMatch.js
@@ -46,7 +46,7 @@ CLMS.model.SpectrumMatch = function(containingModel, participants, crossLinks, p
     this.elution_time_end = +rawMatches[0].e_e;
 
     this.src = +rawMatches[0].src; //for looking up run name
-    this.plfid = +rawMatches[0].plfid; //for looking up peak list file name
+    this.plf = +rawMatches[0].plf; //for looking up peak list file name
     //run name may have come from csv file
     if (rawMatches[0].run_name) {
         this.run_name = rawMatches[0].run_name;
@@ -342,7 +342,7 @@ CLMS.model.SpectrumMatch.prototype.runName = function() {
 }
 
 CLMS.model.SpectrumMatch.prototype.peakListFileName = function() {
-    var runName = this.containingModel.get("peakListFiles").get(this.plfid);
+    var runName = this.containingModel.get("peakListFiles").get(this.plf);
     return runName;
 }
 
